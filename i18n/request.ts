0fa00c2d7b5b0a51b366ed8f4f61bd9fda8cc647
pokebay <148599068+ngocanhnguyentayduong@users.noreply.github.com>
2025-06-30 13:44:26 +0700

    Fix i18n request scope error

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,10 +1,7 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
-export default getRequestConfig(async ({ requestLocale }) => {
-    // This typically corresponds to the `[locale]` segment
-    let locale = await requestLocale;
-
+export default getRequestConfig(async ({ locale }) => {
     // Ensure that a valid locale is used
     if (!locale || !routing.locales.includes(locale as any)) {
         locale = routing.defaultLocale;
